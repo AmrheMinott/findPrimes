333553206e7117128764af672ea05fd3f190ecdc
Amrhe-Minott <60018839+Amrhe-Minott@users.noreply.github.com>
2020-01-29 12:21:27 -0500

    changed variable Names
    variable names were changed during this  minor update
    Edited the print statments to be more meaningful

--- findPrimes.py
+++ findPrimes.py
@@ -6,9 +6,9 @@ import time
     this program function in O(n^2) where each loop of the range of number given by the user
     we go through from the begining comparing each time
 '''
-def findPrimesTheLongWay(LOOPS, start, primesArray):
+def findPrimesTheLongWay(range, start, primesArray):
     isPrime = "is"
-    for i in range(start, LOOPS):
+    for i in range(start, range):
         for y in range(start, i):
             if (i%y == 0): # not a prime
                 isPrime = "not"
@@ -19,9 +19,9 @@ def findPrimesTheLongWay(LOOPS, start, primesArray):
     this function is slighly faster in performance as we are adding the newly found prime numbers
     to a simulated memory (the array) where each new number is compared to the number int he array -> primesArray
 '''
-def findPrimesTheSemiLongWay(LOOPS, start, primesArray):
+def findPrimesTheSemiLongWay(range, start, primesArray):
     isPrime = "is"
-    for i in range(start, LOOPS):
+    for i in range(start, range):
         for y in primesArray:
             if (i%y == 0): # not a prime
                 isPrime = "not"
@@ -34,10 +34,10 @@ def findPrimesTheSemiLongWay(LOOPS, start, primesArray):
     very very similar to the function above except for one line of code. The break statement that one line allows us
     to say "you are not a prime so there is no need t look any futher"
 '''
-def findPrimesTheOptimisedWay(LOOPS, start, primesArray):
+def findPrimesTheOptimisedWay(range, start, primesArray):
     isPrime = "is"
     # highly optimisied using the break statement
-    for i in range(start, LOOPS):
+    for i in range(start, range):
         for y in primesArray: # we are looping through the members of the primes array
             if (i%y == 0): # not a prime
                 isPrime = "not"
@@ -50,9 +50,17 @@ def findPrimesTheOptimisedWay(LOOPS, start, primesArray):
 
 
 # this function prints our arrays in a neat and tidy columns
-def printPrimes(primesArray):
-    columns = 5;
-    rows = int((len(primesArray)/2))+1
+def printPrimes(primesArray, func):
+    if func == 0:
+        print(f"\n\nPrimes found by the long method ")
+    elif func == 1:
+        print(f"\n\nPrimes found by the SEMI-LONG method ")
+    else:
+        print(f"\n\nPrimes found by the OPTIMIZED method")
+
+
+    columns = 5; # this variable is the number of columns I wish to see for the data
+    rows = int((len(primesArray)/2))+1 # calculated using the length of the array divided by 2 plus 1
     printer = ""
     for i in range(rows):
         for x in range(columns):
@@ -63,18 +71,18 @@ def printPrimes(primesArray):
     printer = ""
 
 # this function determines the time of the function we are running
-def timeMe(functionName , func, LOOPS, START_VALUE, primes):
+def timeMe(functionName , func, range, START_VALUE, primes):
     start = time.time()
     if func == 0:
-        findPrimesTheLongWay(LOOPS, START_VALUE, primes)
+        findPrimesTheLongWay(range, START_VALUE, primes)
     elif func == 1:
-        findPrimesTheSemiLongWay(LOOPS, START_VALUE, primes)
+        findPrimesTheSemiLongWay(range, START_VALUE, primes)
     else:
-        findPrimesTheOptimisedWay(LOOPS, START_VALUE, primes)
+        findPrimesTheOptimisedWay(range, START_VALUE, primes)
     end = time.time()
 
     # prints the name of the function plus the time it took to complete execting as well as the number of loops that were done
-    print(f"{functionName} took {end - start} to find primes in the range {LOOPS}")
+    print(f"{functionName} took {end - start} to find primes in the range {range}")
 
 def main():
 
@@ -84,37 +92,34 @@ def main():
     primes = [] # this array holds our value for the primes number we have found
 
 
-    LOOPS = input("Insert value for number of LOOPS\n") # use inputs value for the range they wish to find primes in
-    while not(LOOPS.isdigit()): # error handling is done here where if the user inputs a string the loop continues until an int is inputted
-        LOOPS = input("Insert an integer for number of LOOPS\n")
-    LOOPS = int(LOOPS) # convert the string version of an int to an actual useable int
+    range = input("Insert value for number of range\n") # use inputs value for the range they wish to find primes in
+    while not(range.isdigit()): # error handling is done here where if the user inputs a string the loop continues until an int is inputted
+        range = input("Insert an integer for number of range\n")
+    range = int(range) # convert the string version of an int to an actual useable int
 
 
-    print(f"We are going to find the prime numbers between 1 and {LOOPS} the LONG way")
+    print(f"We are going to find the prime numbers between 1 and {range} the LONG way")
     input("Press enter to execute \"findPrimesTheLongWay\"")
 
-    timeMe("findPrimesTheLongWay" , 0 , LOOPS, START_VALUE, primes)
-    print(f"\n\nPrimes found by the long method ")
-    printPrimes(primes)
+    timeMe("findPrimesTheLongWay" , 0 , range, START_VALUE, primes)
+    printPrimes(primes , 0)
 
 
     primes.clear()
-    print(f"We are going to find the prime numbers between 1 and {LOOPS} the SEMI-LONG way")
+    print("Array cleared...ready for SEMI-LONG")
     input("Press enter to execute \"findPrimesTheSemiLongWay\"")
 
 
-    timeMe("findPrimesTheSemiLongWay" , 1 , LOOPS, START_VALUE, primes)
-    print(f"\n\nPrimes found by the SEMI-LONG method ")
-    printPrimes(primes)
+    timeMe("findPrimesTheSemiLongWay" , 1 , range, START_VALUE, primes)
+    printPrimes(primes , 1)
 
 
     primes.clear()
-    print(f"We are going to find the prime numbers between 1 and {LOOPS} the OPTIMIZED way")
+    print("Array cleared...ready for OPTIMIZED way")
     input("Press enter to execute \"findPrimesTheOptimisedWay\"")
 
-    timeMe("findPrimesTheOptimisedWay" , 2 , LOOPS, START_VALUE, primes)
-    print(f"\n\nPrimes found by the OPTIMIZED method \n\n")
-    printPrimes(primes)
+    timeMe("findPrimesTheOptimisedWay" , 2 , range, START_VALUE, primes)
+    printPrimes(primes , 2)
 
 # we are calling main to run the function we just created
 main()
